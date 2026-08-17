131d76e03592d75ed724e62933fd7ecd240e4811
Dani <jdanign@gmail.com>
2026-08-17 16:47:06 +0200

    Creada nueva acción para obtener la información del HeroStats y configuración del componente para usar esos datos.

--- src/heroes/components/HeroStats.tsx
+++ src/heroes/components/HeroStats.tsx
@@ -14,8 +14,6 @@ export const HeroStats = ({ summary }: Props)=>{
 	if (summary){
 		const { heroCount, villainCount, totalHeroes, strongestHero, smartestHero } = summary;
 
-
-
 		return (
 			<div className="grid grid-cols-2 md:grid-cols-4 gap-4 mb-8">
 				<HeroStatCard 
